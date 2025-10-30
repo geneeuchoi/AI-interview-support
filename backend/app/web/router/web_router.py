6747eb748c3c6eb85c8bc46b3c801e44cd8c99b9
geneeuchoi <chyj2108@naver.com>
2025-10-30 17:28:59 +0900

    feat: Implement feature partial record module

--- backend/app/web/router/web_router.py
+++ backend/app/web/router/web_router.py
@@ -5,11 +5,7 @@ from fastapi.templating import Jinja2Templates
 router = APIRouter()
 templates = Jinja2Templates(directory="app/web/templates")
 
-@router.get("/stt-test", response_class=HTMLResponse)
-async def stt_test(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-@router.get("/email-test", response_class=HTMLResponse)
+@router.get("/prototype", response_class=HTMLResponse)
 async def email_test(request: Request):
-    return templates.TemplateResponse("email.html", {"request": request})
+    return templates.TemplateResponse("prototype.html", {"request": request})
 
